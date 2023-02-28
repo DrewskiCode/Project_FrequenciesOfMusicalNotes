3812f2102b531778f8589e8998b352bdd4af2140
Drew Floyd <112274143+DrewskiCode@users.noreply.github.com>
2023-02-28 12:31:06 -0800

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <cmath>
+#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+    string note = "C0";
+    double refFreq = 16.35;
+    double v = 0;
+    double k = 0;
+    double speedSound = 34500;
+    double twelevethRoot = 1.059463094359;
+    double freq = 0;
+    double wavelength = 0;
+    freq = refFreq * pow(2,v) * pow(twelevethRoot,k);
+    wavelength = speedSound / freq;
+    cout << "Reference Frequency: " << refFreq << endl;
+    cout << "Speed of Sound: " << speedSound << endl;
+    cout << "Note:" << note << " nu(v):" << v << " k:" << k << "Frequency:" << freq  << "Hz " << "Wavelength:" << wavelength << "cm " << endl;
+    return 0;
+}
+
+
